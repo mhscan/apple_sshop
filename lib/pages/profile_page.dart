37839d1a98930c6504d7c35d7992772d5313cbfb
mhscan <mrrezaeeian@gmail.com>
2025-09-02 09:35:04 +0330

    fixed

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import '/constants/colors.dart';
+import '../gen/assets.gen.dart';
+import '../utility/svg.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Kcolor.background,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 42,
+              margin: const EdgeInsets.fromLTRB(42, 4, 42, 32),
+              decoration: BoxDecoration(
+                color: Kcolor.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(),
+                  const Text(
+                    'حساب کاربری',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'SB',
+                      color: Kcolor.primery,
+                    ),
+                  ),
+                  Positioned(
+                    left: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 2),
+                      child: Ksvg(
+                        path: Assets.icons.apple,
+                        size: 28,
+                        color: Kcolor.primery,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'محمد حسین رضاییان',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'SB',
+                color: Kcolor.black,
+              ),
+            ),
+            const SizedBox(height: 4),
+            const Text(
+              '09913292588',
+              style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'SM',
+                color: Kcolor.grey,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 18),
+              child: Wrap(
+                runSpacing: 18,
+                spacing: 14,
+                children: [
+                  
+                ],
+              ),
+            ),
+            const Spacer(),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'اپل شاپ',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'SM',
+                    color: Kcolor.grey,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Text(
+                  '0.0.1',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'GM',
+                    color: Kcolor.grey,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 2),
+            const Text(
+              'zero_one group',
+              style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'GM',
+                color: Kcolor.grey,
+              ),
+            ),
+            const SizedBox(height: 14),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utility/payment_handler.dart
@@ -0,0 +1,53 @@
+import 'package:zarinpal/zarinpal.dart';
+
+import '/utility/string_extension.dart';
+import '/utility/url_handler.dart';
+
+abstract class PaymentHandler {
+  Future<void> initPaymentRquest(int finalPrice);
+  Future<void> sendPaymentRquest();
+  Future<void> verifyPaymentRquest();
+}
+
+class ZarinPalPaymentHandler extends PaymentHandler {
+  final PaymentRequest _paymentRequest = PaymentRequest();
+  String? _authority;
+  String? _status;
+  UrlHandler urlHandler = UrlLauncher();
+
+  @override
+  Future<void> initPaymentRquest(int finalPrice) async {
+    _paymentRequest.setIsSandBox(false);
+    _paymentRequest.setAmount(finalPrice);
+    _paymentRequest.setDescription('tozihat bija');
+    _paymentRequest.setMerchantID('00eb9bdf-b08c-466c-b2ef-6d5b1702e635');
+    _paymentRequest.setCallbackURL('arioapp://appleshop');
+
+
+  }
+
+  @override
+  Future<void> sendPaymentRquest() async {
+    ZarinPal().startPayment(_paymentRequest, (status, paymentGatewayUri) {
+      if (status == 100) {
+        urlHandler.openUrl(paymentGatewayUri!);
+      }
+    });
+  }
+
+  @override
+  Future<void> verifyPaymentRquest() async {
+    ZarinPal().verificationPayment(
+      _status ?? '',
+      _authority ?? '',
+      _paymentRequest,
+      (isPaymentSuccess, refID, paymentRequest) {
+        if (isPaymentSuccess) {
+          print(refID);
+        } else {
+          print('error');
+        }
+      },
+    );
+  }
+}
